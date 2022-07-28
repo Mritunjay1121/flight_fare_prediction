2f1a41f55b28c2016f753831a835ee2635952414
Mritunjay Singh <mritunjay.1121@gmail.com>
2022-07-28 13:36:09 +0530

    Updated

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
 from flask import Flask
 from housing.logger import logging
 import sys
-# from housing.exception import FlightfareException
+from housing.exception import FlightfareException
 app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    logging.info("WE are testing")
+    # logging.info("WE are testing")
 
 
-    # try:
-    #     raise Exception("We are testing exception") 
-    # except Exception as e:
-    #     housing=FlightfareException(e,sys)
-    #     logging.info(housing.error_message)
-    #     logging.info("We are testing logging")
+    try:
+        raise Exception("We are testing exception") 
+    except Exception as e:
+        housing=FlightfareException(e,sys)
+        logging.info(housing.error_message)
+        logging.info("We are testing logging")
     return "I have arrived"
 
 
--- housing/exception/__init__.py
+++ housing/exception/__init__.py
@@ -6,34 +6,34 @@ import sys
 class FlightfareException(Exception):
 
     
-    def __init__(self, error_message:Exception,error_detail:sys):
+    def __init__(self, error_message:Exception,error_detail:sys)->str:
         super().__init__(error_message)
-        self.error_message=FlightfareException.get_detailed_error_message(error_message=error_message,
-                                                                       error_detail=error_detail
-                                                                        )
+        
+        self.error_message=error_message=FlightfareException.get_detailed_error_message(error_message=error_message,error_detail=error_detail)
+                                                                        
 
 
-# @staticmethod
-# def get_detailed_error_message(error_message:Exception,error_detail:sys)->str:
-#     """
-#     error_message: Exception object
-#     error_detail: object of sys module
-#     """
-#     _,_ ,exec_tb = error_detail.exc_info()
-#     exception_block_line_number = exec_tb.tb_frame.f_lineno
-#     # try_block_line_number = exec_tb.tb_lineno
-#     file_name = exec_tb.tb_frame.f_code.co_filename
-#     error_message = f"""
-#     Error occured in script: 
-#     [ {file_name} ] at 
-#     line number: [{exception_block_line_number}] 
-#     error message: [{error_message}]
-#     """
-#     return error_message
+    @staticmethod
+    def get_detailed_error_message(error_message:Exception,error_detail:sys)->str:
+        """
+    #     error_message: Exception object
+    #     error_detail: object of sys module
+    #     """
+        _,_ ,exec_tb = error_detail.exc_info()
+        exception_block_line_number = exec_tb.tb_frame.f_lineno
+        # try_block_line_number = exec_tb.tb_lineno
+        file_name = exec_tb.tb_frame.f_code.co_filename
+        error_message = f"""
+        Error occured in script: 
+        [ {file_name} ] at 
+        line number: [{exception_block_line_number}] 
+        error message: [{error_message}]
+    #     """
+        return error_message
+    # 
+    def __str__(self):
+        return self.error_message
 
-# def __str__(self):
-#     return self.error_message
 
-
-# def __repr__(self) -> str:
-#     return FlightfareException.__name__.str()                                                                    
+    def __repr__(self) -> str:
+        return FlightfareException.__name__.str()                                                                    
