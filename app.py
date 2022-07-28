8da321ff7ab1bd90785547e5560b58f024023a64
Mritunjay Singh <mritunjay.1121@gmail.com>
2022-07-28 13:28:15 +0530

    updated messsage

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
 from flask import Flask
 from housing.logger import logging
 import sys
-from housing.exception import FlightfareException
+# from housing.exception import FlightfareException
 app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    try:
-        raise Exception("We are testing exception")
-    except Exception as e:
-        housing=FlightfareException(e,sys)
-        logging.info(housing.error_message)
-        logging.info("We are testing logging")
-    return "Teri maa ki chut"
+    logging.info("WE are testing")
+
+
+    # try:
+    #     raise Exception("We are testing exception") 
+    # except Exception as e:
+    #     housing=FlightfareException(e,sys)
+    #     logging.info(housing.error_message)
+    #     logging.info("We are testing logging")
+    return "I have arrived"
 
 
 if __name__=="__main__":
